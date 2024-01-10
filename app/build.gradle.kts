b0b622592ef0597472d89020f2e74d9408310b43
Kperez <144041168+PartnersTennis@users.noreply.github.com>
2024-01-10 12:28:07 -0500

    adding custom event to JS

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     implementation("androidx.browser:browser:1.7.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/naranja/webview/MainActivity.kt
+++ app/src/main/java/com/naranja/webview/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.naranja.webview
 
+import android.content.ComponentName
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
+import android.provider.Browser
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsCallback
+import androidx.browser.customtabs.CustomTabsClient
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.browser.customtabs.CustomTabsService
+import androidx.browser.customtabs.CustomTabsServiceConnection
+import androidx.browser.customtabs.CustomTabsSession
+import androidx.browser.customtabs.CustomTabsSessionToken
 import androidx.core.content.ContextCompat
 import com.naranja.webview.databinding.ActivityMainBinding
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
+    private var customTabsSession: CustomTabsSession? = null
+    private var url = "http://192.168.1.47:3000/get-cookies"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -22,21 +37,42 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupViews() {
-        binding.btnTabIntent.setOnClickListener {
-            val url = binding.etWebUrl.text.toString()
-            if (validateUrl(url)) {
-                val builder = CustomTabsIntent.Builder()
-                builder.setToolbarColor(
-                    ContextCompat.getColor(
-                        applicationContext,
-                        R.color.colorPrimaryDark
-                    )
-                )
+        val connection = object : CustomTabsServiceConnection() {
+            override fun onCustomTabsServiceConnected(name: ComponentName, client: CustomTabsClient) {
+                val intentBuilder = CustomTabsIntent.Builder(customTabsSession)
+                intentBuilder.setDefaultColorSchemeParams(CustomTabColorSchemeParams.Builder()
+                    .setToolbarColor(ContextCompat.getColor(this@MainActivity, R.color.colorPrimary))
+                    .build())
+                intentBuilder.setShowTitle(true)
+                intentBuilder.setShareState(CustomTabsIntent.SHARE_STATE_ON)
 
-                val customTabsIntent = builder.build()
-                customTabsIntent.launchUrl(this, Uri.parse(url))
+
+                val customTabsIntent = intentBuilder.build()
+
+                val headers = Bundle().apply {
+                    putString("Authorization", "Bearer asdasd")
+                    putString("redirect-url", "Some redirect url")
+                }
+                customTabsIntent.intent.putExtra("android.intent.extra.REFERRER", Uri.parse("your_referrer_here"))
+                customTabsIntent.intent.putExtra("android.intent.extra.HEADERS", headers)
+
+                customTabsIntent.launchUrl(this@MainActivity, Uri.parse(url))
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                customTabsSession = null
             }
         }
+
+
+        binding.btnTabIntent.setOnClickListener {
+            CustomTabsClient.bindCustomTabsService(
+                this,
+                CustomTabsClient.getPackageName(this, null),
+                connection
+            )
+        }
+
         binding.btnWebview.setOnClickListener {
             val url = binding.etWebUrl.text.toString()
             if (validateUrl(url)) {
@@ -44,6 +80,7 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
         }
+
         binding.btnOpenChrome.setOnClickListener {
             val url = binding.etWebUrl.text.toString()
             url.asUri()?.openInBrowser(this)
